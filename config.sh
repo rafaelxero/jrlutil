58a4bfbbaaf6b198c1666be4827ffe682a04d8a9
Rafael Cisneros <rafael.cisneros@aist.go.jp>
2019-07-30 10:54:21 +0900

    Corrected typo

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 export SRC_DIR=$HOME/src/jrl
 export INSTALL_DIR=$HOME/openrtp
 export NUBMBER_OF_CORES=8
-export BUILD_TYPE=Relsease
+export BUILD_TYPE=Release
 #Specify HMC's build directory only if you want hmc to be rebuilt
 export HMC_BUILD_DIR=
