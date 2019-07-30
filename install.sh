03c3a07038dc09bbe0595b2e450c920538e889a3
Nse Obot <nseobot@buffalo.edu>
2019-07-30 12:46:04 +0900

    add conditional for ros kinetic installation

--- install.sh
+++ install.sh
@@ -43,14 +43,18 @@ git remote add rafaelxero    https://github.com/rafaelxero/Tasks
 git fetch rafaelxero
 git checkout -b topic/HRG --track rafaelxero/topic/HRG
 
-#Install ROS
+
 cd /$SRC_DIR
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-sudo apt update
-sudo apt install ros-kinetic-desktop-full python-rosinstall
-sudo rosdep init
-rosdep update
+if [ "$(rosversion -d)" != "kinetic" ]
+	then
+		#Install ROS
+		sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+		wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+		sudo apt update
+		sudo apt install ros-kinetic-desktop-full python-rosinstall
+		sudo rosdep init
+		rosdep update
+fi
 
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
