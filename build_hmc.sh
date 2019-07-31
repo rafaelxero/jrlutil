566667d3c3f6ab46c9a1f6a0f2c34e37b9d5f761
Nse Obot <nseobot@buffalo.edu>
2019-07-31 15:25:11 +0900

    add -std=c++11 to CMAKE_CXX_FLAGS

--- build_hmc.sh
+++ build_hmc.sh
@@ -4,7 +4,8 @@ echo $HMC_BUILD_DIR
 if [ -n $HMC_BUILD_DIR ]
 	then 
 		cd $HMC_BUILD_DIR
-		cmake -D BUILD_MULTI_CONTACT_MOTION_SOL=ON .	
+		cmake -D BUILD_MULTI_CONTACT_MOTION_SOL=ON .
+		cmake -D CMAKE_CXX_FLAGS="-g -std=c++11" . 	
 		make -j $NUBMBER_OF_CORES
 		make -j$NUBMBER_OF_CORES install
 fi
