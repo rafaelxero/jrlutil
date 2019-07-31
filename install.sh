0b4bf3d80f8f413d5fabba28bdfd881e4b012b6b
Nse Obot <nseobot@buffalo.edu>
2019-07-31 10:11:17 +0900

    check if HOME is at the begining of INSTAL_DIR

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ git checkout topic/HRG
 #Build 
 export PBUI=OFF
 makeLevel="sudo make"
-if [[ $INSTALL_DIR =~ $HOME ]]
+if [[ $INSTALL_DIR == $HOME* ]]
 	then 
 		export PBUI=ON
 		makeLevel="make"
