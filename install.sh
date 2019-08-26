64f6758138345f60862b022de7f04b353ada3fbb
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2019-08-26 18:58:21 +0900

    Use the new defined functions

--- install.sh
+++ install.sh
@@ -58,31 +58,36 @@ failsafe_cmd()
 
 
 echo "Clone repos"
-git clone --recursive https://github.com/jrl-umi3218/Eigen3ToPython
-git clone --recursive  https://github.com/jrl-umi3218/eigen-qld
-git clone --recursive https://gite.lirmm.fr/multi-contact/eigen-lssol.git #Requires login
+clone_repo https://github.com/jrl-umi3218/ Eigen3ToPython
+clone_repo https://github.com/jrl-umi3218/ eigen-qld
 
-cd $SRC_DIR
-git clone --recursive  https://github.com/leethomason/tinyxml2 
-git clone --recursive https://github.com/jrl-umi3218/sch-core-python
+clone_repo https://gite.lirmm.fr/multi-contact/ eigen-lssol #Requires login
+
+clone_repo https://github.com/leethomason/ tinyxml2 
+
+clone_repo https://github.com/jrl-umi3218/ sch-core-python
 cd sch-core-python/
-git remote add rafaelxero https://github.com/rafaelxero/sch-core-python
+failsafe_cmd git remote add rafaelxero https://github.com/rafaelxero/sch-core-python
 git fetch rafaelxero
-git checkout -b topic/HRG --track rafaelxero/topic/HRG
+cond_checkout topic/HRG rafaelxero/topic/HRG
 cd $SRC_DIR
-git clone --recursive https://github.com/jrl-umi3218/SpaceVecAlg
-git clone --recursive https://github.com/jrl-umi3218/RBDyn
+
+
+clone_repo https://github.com/jrl-umi3218/ SpaceVecAlg
+
+clone_repo https://github.com/jrl-umi3218/ RBDyn
 cd RBDyn/
-git remote add rafaelxero https://github.com/rafaelxero/RBDyn
+failsafe_cmd git remote add rafaelxero https://github.com/rafaelxero/RBDyn 
 git fetch rafaelxero
-git checkout -b topic/HRG --track rafaelxero/topic/HRG
+cond_checkout topic/HRG rafaelxero/topic/HRG
 cd $SRC_DIR
-git clone --recursive https://github.com/jrl-umi3218/mc_rbdyn_urdf.git
-git clone --recursive  https://github.com/jrl-umi3218/Tasks.git
+
+clone_repo https://github.com/jrl-umi3218/ mc_rbdyn_urdf
+clone_repo https://github.com/jrl-umi3218/ Tasks
 cd Tasks/
-git remote add rafaelxero    https://github.com/rafaelxero/Tasks
+failsafe_cmd git remote add rafaelxero    https://github.com/rafaelxero/Tasks
 git fetch rafaelxero
-git checkout -b topic/HRG --track rafaelxero/topic/HRG
+cond_checkout topic/HRG rafaelxero/topic/HRG
 
 
 cd $SRC_DIR
@@ -91,9 +96,9 @@ if [ "$(rosversion -d)" != "kinetic" ]
 		echo "Install ROS"
 		sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 		wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
-		sudo apt update
+		failsafe_cmd sudo apt update
 		sudo apt install ros-kinetic-desktop-full python-rosinstall
-		sudo rosdep init
+		failsafe_cmd sudo rosdep init
 		rosdep update
                 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
                 source /opt/ros/kinetic/setup.bash            
@@ -106,9 +111,8 @@ then
 fi
 
 echo "Create Catkin Workspace"
-mkdir -p catkin_ws/src
-cd catkin_ws/src
-catkin_init_workspace
+soft_mkcd catkin_ws/src
+failsafe_cmd catkin_init_workspace
 cd ..
 catkin_make
 echo "source $SRC_DIR/catkin_ws/devel/setup.bash" >> ~/.bashrc
@@ -116,27 +120,26 @@ source $SRC_DIR/catkin_ws/devel/setup.bash
 
 echo "Clone catkin workspace repos"
 cd $SRC_DIR/catkin_ws/src/
-git clone --recursive https://gite.lirmm.fr/mc-hrp2/hrp2_drc
-mkdir hrp5
-cd hrp5/
-git clone --recursive https://gite.lirmm.fr/mc-hrp5/hrp5_p_description.git
+clone_repo https://gite.lirmm.fr/mc-hrp2/ hrp2_drc
+soft_mkcd hrp5/
+clone_repo https://gite.lirmm.fr/mc-hrp5/ hrp5_p_description
 #cd $SRC_DIR/catkin_ws/src/
-#git clone --recursive https://gite.lirmm.fr/mc-hrp4/hrp4
+#clone_repo --recursive https://gite.lirmm.fr/mc-hrp4/ hrp4
 cd $SRC_DIR/catkin_ws/src/
-git clone --recursive https://gite.lirmm.fr/multi-contact/mc_rtc_ros_data
+clone_repo https://gite.lirmm.fr/multi-contact/ mc_rtc_ros_data
 
 
 cd $SRC_DIR/
 git config --global url."https://gite.lirmm.fr/".insteadOf git@gite.lirmm.fr:
-git clone --recursive https://gite.lirmm.fr/multi-contact/mc_rtc.git
+clone_repo  https://gite.lirmm.fr/multi-contact/ mc_rtc
 cd mc_rtc/
 git checkout topic/HRG
 cd ..
-git clone --recursive https://gite.lirmm.fr/mc-hrp5/mc_hrp5_p.git
+clone_repo https://gite.lirmm.fr/mc-hrp5/ mc_hrp5_p
 cd mc_hrp5_p/
 git checkout topic/HRG
 cd ..
-git clone --recursive https://gite.lirmm.fr/multi-contact/mc_openrtm.git
+clone_repo  https://gite.lirmm.fr/multi-contact/ mc_openrtm
 cd mc_openrtm/
 git checkout topic/HRG
 
@@ -157,30 +160,28 @@ wget https://github.com/nanomsg/nanomsg/archive/1.1.5.zip
 unzip 1.1.5.zip
 #From nanomsg-1.1.5 README’s Quick Installation.
 cd nanomsg-1.1.5/
-mkdir build
-cd build
+soft_mkcd build
 cmake ..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake --build .
 ctest .
-$makeLevel cmake --build . --target install
+echo "$makeLevel cmake --build . --target install"
+cmake --build . --target install
 
 cd $SRC_DIR/Eigen3ToPython/
-mkdir build
-cd build/
+soft_mkcd build
 cmake ..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
 cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE .
 sudo apt-get install python-pip
-pip install Cython
+pip install Cython 
 pip install coverage
 make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/eigen-qld/
-mkdir build
-cd build/
+soft_mkcd build
 cmake ..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -189,8 +190,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/eigen-lssol/
-mkdir build
-cd build/
+soft_mkcd build
 cmake ..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -199,8 +199,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR//tinyxml2/
-mkdir build
-cd build/
+soft_mkcd build
 cmake ..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -209,8 +208,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/SpaceVecAlg/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -219,8 +217,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/sch-core-python/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -229,8 +226,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/RBDyn/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -239,8 +235,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/mc_rbdyn_urdf/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -249,8 +244,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/Tasks/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -264,8 +258,7 @@ catkin_make
 
 echo "building mc_rtc"
 cd $SRC_DIR/mc_rtc/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -274,8 +267,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/mc_hrp5_p/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -284,8 +276,7 @@ make -j $NUBMBER_OF_CORES
 $makeLevel -j$NUBMBER_OF_CORES install
 
 cd $SRC_DIR/mc_openrtm/
-mkdir build
-cd build/
+soft_mkcd build
 cmake -B. -H..
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" .
 cmake -D PYTHON_BINDING_USER_INSTALL=$PBUI .
@@ -295,7 +286,7 @@ $makeLevel -j$NUBMBER_OF_CORES install
 
 echo "cloning mc_rtc_ros"
 cd $SRC_DIR/catkin_ws/src
-git clone --recursive https://gite.lirmm.fr/multi-contact/mc_rtc_ros
+clone_repo https://gite.lirmm.fr/multi-contact/ mc_rtc_ros
 
 echo "building catkin workspace"
 cd $SRC_DIR/catkin_ws
