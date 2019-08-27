7d68d2b357102a3310cb6bbfa7eca86b4b8e5a3f
Mehdi Benallegue <mehdi.benallegue@gmail.com>
2019-08-27 19:03:51 +0900

    Rewrite build_hmc

--- build_hmc.sh
+++ build_hmc.sh
@@ -1,11 +1,18 @@
-./config.sh
 source config.sh
-echo $HMC_BUILD_DIR
-if [ -n $HMC_BUILD_DIR ]
-	then 
-		cd $HMC_BUILD_DIR
-		cmake -D BUILD_MULTI_CONTACT_MOTION_SOL=ON .
-		cmake -D CMAKE_CXX_FLAGS="-g -std=c++11" . 	
-		make -j $NUBMBER_OF_CORES
-		make -j$NUBMBER_OF_CORES install
+
+
+containsElement () {
+
+if [ $REBUILD_HMC != 0 ] then 
+    cd $DRCUTIL_DIR
+    export CMAKE_ADDITIONAL_OPTIONS=($CMAKE_ADDITIONAL_OPTIONS -D BUILD_MULTI_CONTACT_MOTION_SOL=ON -D CMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -std=c++11")
+    source config.sh
+    if [[ "${CMAKE_ADDITIONAL_OPTIONS[@]}" =~ "c++11" ]]; then #checks if the options are taken into account
+        ./install hmc2
+    else
+        echo "Error in build_hmc.sh"
+        echo "Please check that the value of CMAKE_ADDITIONAL_OPTIONS is not errased in $DRCUTIL_DIR/config.sh"
+        echo "After fixing the issue. YOu may directly run this script ./build_hmc.sh"
+    fi
+    
 fi
--- config.sh
+++ config.sh
@@ -2,5 +2,6 @@ export SRC_DIR=$HOME/src/jrl
 export INSTALL_DIR=$HOME/openrtp
 export NUBMBER_OF_CORES=8
 export BUILD_TYPE=Release
-#Specify HMC's build directory only if you want hmc to be rebuilt
-export HMC_BUILD_DIR=
+export REBUILD_HMC=0
+#Specify drcutil build directory only if you want hmc to be rebuilt
+export DRCUTIL_DIR=
